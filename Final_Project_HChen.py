ca5a0b56788efa2b4efd6aae6506c0f0202bc830
hannahchn <86988694+hannahchn@users.noreply.github.com>
2021-07-15 23:31:29 -0400

    Update Final_Project_HChen.py

--- Final_Project_HChen.py
+++ Final_Project_HChen.py
@@ -13,7 +13,7 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 import pydeck as pdk
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from PIL import Image, ImageFont, ImageDraw
 
 # Set the pages to wide layout
